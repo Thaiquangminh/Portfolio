d1e3037d6e00d16d8352cfc4fad200fd8bd5f7c6
Internship-Thai Quang Minh <minhtq@vsii.com>
2023-06-07 00:30:22 +0700

    feature-experience-and-services-and-portfolio

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Contact from './components/contact/Contact';
 import Experience from './components/experience/Experience';
 import Header from './components/header/Header';
 import NavBar from './components/nav/NavBar';
+import Portfolio from './components/portfolio/Portfolio';
 import Services from './components/services/Services';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Experience />
       <Services />
+      <Portfolio />
       <Contact />
     </div>
   );
