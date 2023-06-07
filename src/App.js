094d724fa3c2dea0b60601983da6fe48ae6a8a8b
Internship-Thai Quang Minh <minhtq@vsii.com>
2023-06-08 01:13:00 +0700

    feature-footer-and-fix-ui-header-navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
 import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import NavBar from './components/nav/NavBar';
 import Portfolio from './components/portfolio/Portfolio';
@@ -18,6 +19,7 @@ function App() {
       <Portfolio />
       <Testimonials />
       <Contact />
+      <Footer />
     </div>
   );
 }
